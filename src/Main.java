44c153ba6e978f885262a24d129efe9899a9dacb
CamiloP <juancamilopadillabulla@gmail.com>
2026-02-19 12:35:11 -0500

    Datos

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Juan Camilo Padilla Bulla");
+        System.out.println("zipaquira");
+        System.out.println("17 años");
+        System.out.println("no tengo mascota");
+
         }
     }
-}
